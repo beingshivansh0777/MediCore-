8a692e61229fe3c84b11f7526775889b01d9f60f
Shivansh Mishra <luckymishra2625@gmail.com>
2026-05-09 11:29:52 +0530

    Improved cors functionality

--- server/server.js
+++ server/server.js
@@ -18,21 +18,22 @@ const allowedOrigins = [
   process.env.ADMIN_URL,
 ].filter(Boolean);
 
-//CORS
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.includes(origin)) {
-//         return callback(null, true);
-//       } else {
-//         return callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+// app.use(cors({ origin: "*" }));
 
 // Middlewares
 app.use(express.json({ limit: "20kb" }));
